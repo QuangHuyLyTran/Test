2aba4a3c21b573be19b726cb2236ddc03b2ae2cf
Quang <quanghuylytran@gmail.com>
2022-05-03 19:48:58 +0700

    HuyQuang

---
+++ Assets/Scripts/DemoAPoints.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoAPoints : MonoBehaviour
+{
+    public GameObject projectile;
+
+    public GameObject clone;
+
+    void Update()
+    {
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                clone =
+                    Instantiate(projectile,
+                    transform.position,
+                    transform.rotation) as
+                    GameObject;
+            }
+        }
+    }
+}
